bc4891798f01e76b6566d7674e9e8caea07d8312
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2016-10-10 20:14:59 +0100

    Added exercise 16 to the day exercise file

--- day-02/Exercises-Day-02.groovy
+++ day-02/Exercises-Day-02.groovy
@@ -177,4 +177,63 @@ if (guessNum == numberToGuess) {
  * Hint: remember that you can use .substring() to get the elements of a String.
  */
 
- 
+int playerOneCounter;
+int playerTwoCounter;
+
+String userCombination;
+
+println "==== Let's play Rock, Paper, Scissors! ====";
+println "Pick a combination between PP, PR, PS, RP, RR, RS, SP, SR, SS\n";
+
+while (playerOneCounter < 3 && playerTwoCounter < 3) {
+
+	// Read input!
+	print "Go on, pick one: ";
+
+	userCombination = System.console().readLine();
+
+	switch (userCombination) {
+	 	case "PP":
+	 		println "Draw";
+	 		break;
+	 	case "PR":
+	 		println "Player One wins";
+	 		playerOneCounter++;
+	 		break;
+	 	case "PS":
+	 		println "Player Two wins";
+	 		playerTwoCounter++;
+	 		break;
+	 	case "RP":
+	 		println "Player Two wins";
+	 		playerTwoCounter++;
+	 		break;
+	 	case "RR":
+	 		println "Draw";
+	 		break;
+	 	case "RS":
+	 		println "Player One wins";
+	 		playerOneCounter++;
+	 		break;
+	 	case "SP":
+	 		println "Player One wins";
+	 		playerOneCounter++;
+	 		break;
+	 	case "SR":
+	 		println "Player Two wins";
+	 		playerTwoCounter++;
+	 		break;
+	 	case "SS":
+	 		println "Draw";
+	 		break;
+	 	default:
+	 		println "Invalid input";
+	 		break;
+	}
+}
+
+if (playerOneCounter > playerTwoCounter) {
+	println "\n === Player One wins! ===\n";
+} else {
+	println "\n === Player Two wins! ===\n";
+}
