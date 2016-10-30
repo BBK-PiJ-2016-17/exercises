526dece14f651e45d0db408be601f820fc0d4851
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2016-10-30 19:59:40 +0000

    Starting Day 5 exercises from the notes

---
+++ day-05/Employees.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * Write a small program that asks for the names and IDs of all employees in a
+ * small company, and store them in an array of integers and an array of Strings.
+ * The company has 10 employees.
+ * Use a loop to go through both arrays and print the names and IDs of those
+ * employees whose ID is less than 1000. Use another loop to print the names and
+ * IDs of those employees whose name starts with “J” or “S”.
+ *
+ */
+
+// import java.*;
+
+public class Employees {
+
+	private String name;
+	private int id;
+
+	public Employee(String, int) {
+		this.name = name;
+		this.id = id;
+	}
+
+	String[] listOfEmployees = new Employee[];
+
+	Employee listOfEmployees[0] = "John Smith", "1999";
+
+}
