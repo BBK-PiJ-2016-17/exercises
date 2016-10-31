6ff2b9ab54ecfa75c85bbe78f52fded6c4453ff9
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2016-10-31 22:32:39 +0000

    More exercises form day 5

--- day-05/ArrayCopier.java
+++ day-05/ArrayCopier.java
@@ -2,17 +2,26 @@ public class ArrayCopier {
 
 	private int[] src;
 	private int[] dst = {0, 0, 0, 0, 0};
+	private int i = 0;
+	private int shorter = 0; // get the shorter index to loop over
 
 	public int[] copy(int[] src, int[] dst) {
 
 		this.src = src;
 		this.dst = dst;
+		// this.shorter = shorter;
 
-		dst[0] = src[0];
-		dst[1] = src[1];
-		dst[2] = src[2];
-		dst[3] = src[3];
-		dst[4] = src[4];
+		// get the shorter number?
+		for (i = 0; i < shorter; i++) {
+
+			dst[i] = src[i];
+		}
+
+		// dst[0] = src[0];
+		// dst[1] = src[1];
+		// dst[2] = src[2];
+		// dst[3] = src[3];
+		// dst[4] = src[4];
 
 		return dst;
 
--- day-05/ArrayCopierLauncher.java
+++ day-05/ArrayCopierLauncher.java
@@ -3,15 +3,48 @@ public class ArrayCopierLauncher {
 	public static void main(String[] args) {
 
 		int i = 0;
+		int shorter = 0;
 		int[] src = {3, 4, 5, 6, 7};
-		int[] dst = {0, 0, 0, 0, 0};
+		int[] dst = {0, 0, 0, 0};
 
 		ArrayCopier copier = new ArrayCopier();
 	
 		copier.copy(src, dst);
 
-		for (i = 0; i < 5; i++) {
-			System.out.print(dst[i] + ", ");
+		if (src.length < dst.length) {
+
+			shorter = src.length;
+
+			System.out.println("\nsrc[] is smaller than dst[]" +
+				"\nI'm copying over everything, but there still " +
+				(dst.length - src.length) + " space(s) available in dst[].\n");
+
+			// Copy all elements over and leave empty space on dst[]
+			for (i = 0; i < src.length; i++) {
+				System.out.print("/ " + dst[i] + " /");
+			}
+		} else if (src.length > dst.length) {
+
+			shorter = dst.length;
+
+			System.out.println("\nsrc[] is bigger than dst[]" +
+				"\nI'm only copying over as many elements as dst[] can fit.\n");
+
+			// Only copy as many elements as dst[] can fit
+			for (i = 0; i < dst.length; i++) {
+				System.out.print("/ " + dst[i] + " /");
+			}
+		} else if (src.length == dst.length) {
+
+			shorter = src.length; // in this case they are the same
+
+			System.out.println("\nsrc[] and dst[] are of equal length." +
+				"\nI'm copying over everything.\n");
+
+			// Duplicate
+			for (i = 0; i < dst.length; i++) {
+				System.out.print("/ " + dst[i] + " /");
+			}
 		}
-	}
+ 	}
 }
