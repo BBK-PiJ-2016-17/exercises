2d0714559bf6ca9cca57dee8cff7ee984c4e0838
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2016-11-13 12:08:52 +0000

    Added reading from user input

--- Java-a-Beginner-Guide-Exercises/Hypot.java
+++ Java-a-Beginner-Guide-Exercises/Hypot.java
@@ -11,11 +11,11 @@ public class Hypot {
 
         System.out.println();
 
-        System.out.println("What's the lenght of side 1 of your triangle?");
+        System.out.print("What's the lenght of side 1 of your triangle? ");
         String xString = System.console().readLine();
         x = Integer.parseInt(xString);
 
-        System.out.println("What's the lenght of side 2 of your triangle?");
+        System.out.print("What's the lenght of side 2 of your triangle? ");
         String yString = System.console().readLine();
         y = Integer.parseInt(yString);
 
