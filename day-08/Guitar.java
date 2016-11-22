6940ac03eb097a4243555308eaad152b2ce53fed
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2016-11-22 09:40:24 +0000

    Updated Ex 5 - Day 08 / Multiple inheritance

--- day-08/Guitar.java
+++ day-08/Guitar.java
@@ -1,3 +1,11 @@
+/**
+ * Ex 5 - Multiple inheritance
+ * Create a class MusicalInstrument with a method play(). Now create another
+ * class WoodenObject with a method burn().
+ * Create a class Guitar that is at the same time a musical instrument and a
+ * wooden object. How would you do it in Java?
+*/
+
 public class Guitar {
     // In order to have both the play() method from MusicalInstrument
     // And the burn() method from WoodenObject class
--- day-08/WoodenObject.java
+++ day-08/WoodenObject.java
@@ -1,3 +1,11 @@
+/**
+ * Ex 5 - Multiple inheritance
+ * Create a class MusicalInstrument with a method play(). Now create another
+ * class WoodenObject with a method burn().
+ * Create a class Guitar that is at the same time a musical instrument and a
+ * wooden object. How would you do it in Java?
+*/
+
 public class WoodenObject {
 
     private int temperature = 0;
