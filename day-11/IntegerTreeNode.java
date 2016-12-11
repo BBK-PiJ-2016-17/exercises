1f00206601e5d73d23e1629d407b9df99a532471
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2016-12-11 19:08:10 +0000

    Ex 1.1 - Day 11

--- day-11/IntegerTreeNode.java
+++ day-11/IntegerTreeNode.java
@@ -17,8 +17,15 @@ public class IntegerTreeNode {
     IntegerTreeNode left;
     IntegerTreeNode right;
     
+    // Constructor
+    public IntegerTreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
     // ... methods would be here
-    public add(int newNumber) {
+    public void add(int newNumber) {
         if (newNumber > this.value) {
             if (right == null) {
                 right = new IntegerTreeNode(newNumber);
@@ -52,20 +59,68 @@ public class IntegerTreeNode {
         }
     }
 
-    public int getMax(int max) {
-        return max;
+    public int getMax() {
+        if (right == null) {
+            return this.value;
+        } else {
+            return right.getMax();
+        }
     }
 
-    public int getMin(int min) {
-        return min;
+    public int getMin() {
+        if (left == null) {
+            return this.value;
+        } else {
+            return left.getMin();
+        }
+    }
+
+    // depth
+    public int getDepth() {
+        int leftDepth = 0;
+        if (left != null) {
+            leftDepth = left.getDepth();
+            // could add 1 here and remove it from the return
+        }
+        int rightDepth = 0;
+        if (right != null) {
+            rightDepth = right.getDepth();
+            // could add 1 here and remove it from the return
+        }
+
+        if (leftDepth > rightDepth) {
+            return 1 + leftDepth;
+        } else {
+            return 1 + rightDepth;
+        }
     }
 
     public static void main(String[] args) {
-        IntegerTreeNode test = new IntegerTreeNode();
-        test.launch();
+        IntegerTreeNode root = new IntegerTreeNode(9);
+        root.launch();
     }
 
     public void launch() {
+
+        // Once initialised, add more nodes
+        add(2);
+        add(13);
+        add(1);
+        add(5);
+        add(17);
+        add(-1);
+        add(1000);
+        add(24);
+
+        System.out.println("The smallest element of the tree is: " + getMin());
+        System.out.println("The biggest element of the tree is: " + getMax());
+
+        System.out.println("Does the tree contain no. 27? " + contains(27));
+        System.out.println("Does the tree contain no. 13? " + contains(13));
+
+        getDepth();
+
+        System.out.println("Depth of the tree: " + getDepth());
 
     }
 }
