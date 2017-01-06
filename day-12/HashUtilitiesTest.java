72796618c9e11192d2660f7fca39e776f0d38957
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-06 16:50:57 +0000

    Added more tests

--- day-12/HashUtilitiesTest.java
+++ day-12/HashUtilitiesTest.java
@@ -7,12 +7,64 @@ import org.junit.Test;
 
 public class HashUtilitiesTest {
 
+    // 1
     @Test
     public void testsHash() {
         HashUtilities hash = new HashUtilities();
         int input = 23456789;
         int output = hash.shortHash(input);
         int expected = 789;
+        assertEquals(output, expected);
+    }
+
+    // 2
+    @Test
+    public void testsHash2() {
+        HashUtilities hash = new HashUtilities();
+        int input = 15158;
+        int output = hash.shortHash(input);
+        int expected = 158;
+        assertEquals(output, expected);
+    }
+
+    // 3
+    @Test
+    public void testsZero() {
+        HashUtilities hash = new HashUtilities();
+        int input = 0;
+        int output = hash.shortHash(input);
+        int expected = 0;
+        assertEquals(output, expected);
+    }
+
+    // 4
+    @Test
+    public void tests1000() {
+        HashUtilities hash = new HashUtilities();
+        int input = 1000;
+        int output = hash.shortHash(input);
+        int expected = 0;
+        assertEquals(output, expected);
+    }
+
+    // 5
+    // Try to fail - It fails, good!
+    // @Test
+    // public void tests1000again() {
+    //     HashUtilities hash = new HashUtilities();
+    //     int input = 1000;
+    //     int output = hash.shortHash(input);
+    //     int expected = 1;
+    //     assertEquals(output, expected);
+    // }
+
+    // 6
+    @Test
+    public void tests1001() {
+        HashUtilities hash = new HashUtilities();
+        int input = 1001;
+        int output = hash.shortHash(input);
+        int expected = 1;
         assertEquals(output, expected);
     }
 
