f58cb5c3994d2c8586c0bda1dd4a5681aa22b3a1
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-09 19:48:23 +0000

    Ex 4.1 - First test isEmptyTest()

--- day-12/StringStack/src/StringStackTest.java
+++ day-12/StringStack/src/StringStackTest.java
@@ -2,6 +2,11 @@
  * Created by ginestraferraro on 09/01/2017.
  */
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class StringStackTest {
@@ -12,9 +17,40 @@ public class StringStackTest {
     //      String peek()
     //      boolean isEmpty()
 
+    @Before
+    public void setUp() {
+        // Execute before each test
+        System.out.println("@Before setUp()");
+        ArrayStringStack stringStackTest = new ArrayStringStack();
+    }
+
+    @Ignore
     @Test
     public void pushTest() {
 
+    }
+
+    @Ignore
+    @Test
+    public void popTest() {
+
+    }
+
+    @Ignore
+    @Test
+    public void peekTest() {
+
+    }
+
+    @Test
+    public void isEmptyTest() {
+        assertTrue("failure - should be true", true);
+    }
+
+    @After
+    public void tearDown() {
+        // Execute after each test
+        System.out.println("@After tearDown()");
     }
 
 }
