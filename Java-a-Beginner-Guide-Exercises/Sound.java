a6034d53dc5d128532796b98343a91e4153ac816
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-09 15:03:19 +0000

    Asking users for the time passed, rather than fixing a time

--- Java-a-Beginner-Guide-Exercises/Sound.java
+++ Java-a-Beginner-Guide-Exercises/Sound.java
@@ -6,9 +6,20 @@
 
 public class Sound {
     public static void main(String args[]) {
-        double dist;
-        dist = 7.2 * 1100;
-        System.out.println("The lightning is " + dist +
-        " feet away.");
+
+        System.out.print("\nHow many seconds between the lighting and the thunder? ");
+        String str = System.console().readLine();
+
+        double time = Double.parseDouble(str);
+
+        System.out.println("\n==== Distance in feet ====\n");
+
+        double dist = time * 1100; // feet per second
+        System.out.println("The lightning is " + dist + " feet away.");
+
+        System.out.println("\n==== Distance in meters ====\n");
+
+        dist = time * 343; // meters per second
+        System.out.println("The lightning is " + dist + " meters away.");
     }
 }
