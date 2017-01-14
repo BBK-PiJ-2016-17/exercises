6cff3806b1e8ad84fe9867b63fd2e4a45abfe22b
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-14 12:00:50 +0000

    Starting exercises for day 13

---
+++ day-13/Library/src/BookImpl.java
@@ -0,0 +1,9 @@
+/**
+ * Day 13 - Practicing TDD
+ *
+ *
+ * @author Ginestra Ferraro
+ */
+
+public class BookImpl {
+}
---
+++ day-13/Library/src/LibraryTest.java
@@ -0,0 +1,13 @@
+/**
+ * Day 13 - Practicing TDD
+ * Tests
+ *
+ * @author Ginestra Ferraro
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class LibraryTest {
+
+}
