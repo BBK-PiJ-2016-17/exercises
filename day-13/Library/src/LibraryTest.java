0871392d3b73056e2d474c69b6612f7cbd064c33
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-14 13:36:34 +0000

    Trying to fix the @Before issue

--- day-13/Library/src/LibraryTest.java
+++ day-13/Library/src/LibraryTest.java
@@ -6,26 +6,28 @@
  */
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+// import static org.junit.Assert.assertFalse;
+// import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
+// import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
+// import org.junit.Ignore;
 import org.junit.Test;
 
 public class LibraryTest {
 
     @Before
-    public void MockBook() {
+    public void setUp() {
         BookImpl firstBook = new BookImpl("Mark Twain", "The Million Pound Bank Note");
-
-        System.out.println("Author: " + firstBook.getAuthor() + "/nTitle: " + firstBook.getTitle());
     }
 
-    @Ignore
     @Test
     public void testTitle() {
+
+        String output = "Mark Twain";
+        String input = firstBook.getAuthor();
+
+        assertEquals(output, input);
 
     }
 }
