14bbb3cc306bc0c7bbb4528d555da72cf76e089b
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-14 15:25:29 +0000

    Adding User - Ex 1.2

--- day-13/Library/src/LibraryTest.java
+++ day-13/Library/src/LibraryTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 // import static org.junit.Assert.assertFalse;
 // import static org.junit.Assert.assertTrue;
 
-// import org.junit.After;
+import org.junit.After;
 import org.junit.Before;
 // import org.junit.Ignore;
 import org.junit.Test;
@@ -17,16 +17,19 @@ import org.junit.Test;
 public class LibraryTest {
 
     private Book firstBook, secondBook, thirdBook; // Important: define the object here, to make it visible to @Test
+    private User userOne, userTwo;
 
     @Before
     public void setUp() {
         firstBook = new BookImpl("Mark Twain", "The Million Pound Bank Note");
         secondBook = new BookImpl("Fred Vargas", "The Three Evangelists");
         thirdBook = new BookImpl("Sadie Smith", "White Teeth");
+
+        userOne = new UserImpl("This guy");
     }
 
     @Test
-    public void testAuthor() {
+    public void authorTest() {
 
         String output = "Mark Twain";
         String input = firstBook.getAuthor();
@@ -36,12 +39,27 @@ public class LibraryTest {
     }
 
     @Test
-    public void testTitle() {
+    public void titleTest() {
 
         String output = "White Teeth";
         String input = thirdBook.getTitle();
 
         assertEquals(output, input);
 
+    }
+
+    @Test
+    public void userTest() {
+
+        String nameOut = "John Smith";
+        String nameIn = userOne.getName();
+
+    }
+
+    @After
+    public void cleanUp() {
+        firstBook = null;
+        secondBook = null;
+        thirdBook = null;
     }
 }
