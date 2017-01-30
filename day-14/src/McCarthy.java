990d0fb5718bcd7a6fb802e01f60885a0b8bb4fc
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-30 19:56:33 +0000

    Adding McCarthy ex

--- day-14/src/McCarthy.java
+++ day-14/src/McCarthy.java
@@ -11,22 +11,53 @@
  */
 public class McCarthy {
 
+    // I've added a counter to check the relation between the number 101 and the
+    // times that the loop gets executed.
+    // It turns out it executes (101 - n) times
+    public int counter = 0;
+
     public static void main(String[] args) {
         McCarthy test = new McCarthy();
         test.launch();
     }
 
     public void launch() {
+        System.out.println("\n=== mcCarthy 50 ===");
         mcCarthy91(50);
+        counter = 0;
+        System.out.println("\n=== mcCarthy 73 ===");
+        mcCarthy91(73);
+        counter = 0;
+        System.out.println("\n=== mcCarthy 95 ===");
+        mcCarthy91(95);
+        counter = 0;
+
+        // Corner cases
+
+        System.out.println("\n=== McCarthy 101 ===");
+        mcCarthy91(101);
+        counter = 0;
+
+        System.out.println("\n=== McCarthy 91 ===");
+        mcCarthy91(91);
     }
 
     public int mcCarthy91(int n) {
+        System.out.println("\tLine 29 - n is: " + n);
         if (n > 100) {
+            System.out.print("\n> 100: n is " + n + " / We subtract 10 = ");
             System.out.println(n - 10);
             return n - 10;
         }
 
-        return mcCarthy91(mcCarthy91(n+11));
+        // System.out.println("This: " + mcCarthy91(mcCarthy91(n + 11)));
+
+        counter++;
+        System.out.println(counter + "\tLine 38 - n is: " + n);
+        return mcCarthy91(mcCarthy91(n + 11)); // Double recursion
+
+        // I don't understand why it ends always at 101.
+        // (Because it returns 91 - But why 91?)
 
     }
 
