4e113c08e21fba25cd2b8d80b254e4d60402fb26
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-02-13 20:25:24 +0000

    Still working on Ex 3

--- day-16/src/ShowContent.java
+++ day-16/src/ShowContent.java
@@ -23,52 +23,55 @@ public class ShowContent {
     public static void main(String[] args) {
         ShowContent openIfExists = new ShowContent();
 
-        openIfExists.launch();
-    }
-
-    public void launch() {
+        String thisFile = null;
 
         try {
+            openIfExists.launch();
 
-            System.out.print("What file do you want me to open? ");
-            String thisFile = System.console().readLine();
-
-            // Loop through the files in this folder and check if any matches the
-            // name (need to remove extension?)
-
-            File here = new File("/Users/ginestra/Dropbox/MSc/MSc-PiJ-16-17" +
-                    "/exercises/day-16/src");
-
-            String[] files = here.list();
-
-            for (String file : files) {
-
-                if (file.equals(thisFile)) {
-                    // Print the content of the file
-                    File userFile = new File(thisFile);
-                    try (BufferedReader in = new BufferedReader(new FileReader(userFile))) {
-
-                        String line;
-                        while ((line = in.readLine()) != null) {
-                            System.out.println(line);
-                        }
-                    } catch (FileNotFoundException ex) {
-                        System.out.println("File " + userFile + " does not exist.");
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }
-
-            // Throw the exception!
             throw new FileNotFoundException(thisFile);
-
         } catch (FileNotFoundException ex) {
-            System.out.println("There is no file with that name.");
+            System.out.println("File " + thisFile + " does not exists.");
             ex.printStackTrace();
         } finally {
-            System.out.println("\n--- That's all folks (finally! Pun " +
-                    "intended) ---\n");
+            System.out.println("\n--- That's all folks ---\n");
+        }
+    }
+
+    public void launch() throws FileNotFoundException {
+
+        System.out.print("What file do you want me to open? ");
+        String thisFile = System.console().readLine();
+
+        // Loop through the files in this folder and check if any matches the
+        // name (need to remove extension?)
+
+        File here = new File("/Users/ginestra/Dropbox/MSc/MSc-PiJ-16-17" +
+                "/exercises/day-16/src");
+
+        String[] files = here.list();
+
+        for (String file : files) {
+
+            if (file.equals(thisFile)) {
+
+                System.out.println("\n===== Printing " + thisFile + " =====\n");
+
+                // Show the content of the file
+                File userFile = new File(thisFile);
+                try (BufferedReader in = new BufferedReader(new FileReader(userFile))) {
+
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        System.out.println(line);
+                    }
+
+                } catch (FileNotFoundException ex) {
+                    System.out.println("File " + thisFile + " does not exist." +
+                            " 2");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
 
     }
