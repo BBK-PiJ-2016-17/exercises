0d1e909d98e289dc02d539dfc763c599dc4ffc28
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-05-01 11:51:26 +0100

    Some clarifications on stack

--- day-05/stack-practice/MyStack.java
+++ day-05/stack-practice/MyStack.java
@@ -3,12 +3,11 @@ import java.util.Arrays;
 public class MyStack {
     private int size;
     private String myArray[];
-    private int top;
+    private int top = -1;
 
     public MyStack(int size) {
         this.size = size;
         this.myArray = new String[size];
-        this.top = top;
     }
 
     public void push(String item) {
@@ -24,7 +23,9 @@ public class MyStack {
     public String pop() {
         if (top >= 0) {
             System.out.println("Removing one item from the stack: \"" + myArray[top] + "\"");
-            return myArray[top--];
+            myArray[top] = null;
+            top--;
+            return myArray[top];
         }
         
         System.out.println("Pop Stack is empty");
@@ -47,21 +48,99 @@ public class MyStack {
 
         MyStack thisStack = new MyStack(10);
 
-        System.out.println(thisStack.size);
+        System.out.println("Size of the stack: " + thisStack.size);
 
         thisStack.peek();
+
+        System.out.println();
 
         thisStack.push("First item");
+        System.out.print("\t");
         thisStack.peek();
-        thisStack.push("Second item");
-        thisStack.peek();
-        thisStack.push("Third item");
+        System.out.println("\tThe top index is: " + thisStack.top);
 
+        System.out.println();
+        
+        thisStack.push("Second item");
+        System.out.print("\t");
         thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        thisStack.push("Third item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        thisStack.push("Fourth item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        thisStack.push("Fifth item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        thisStack.push("Sixth item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        thisStack.push("Seventh item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        thisStack.push("Eighth item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        thisStack.push("Ninth item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        thisStack.push("Tenth item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
+
+        // Stack should be full now
+        thisStack.push("Tenth item");
+        System.out.print("\t");
+        thisStack.peek();
+        System.out.println("\tThe top index is: " + thisStack.top);
+
+        System.out.println();
 
         thisStack.pop();
 
+        System.out.print("\t");
         thisStack.peek();
+
+
+        System.out.println();
+        System.out.println("At index 0: " + thisStack.myArray[0]);
+        System.out.println("At index 9: " + thisStack.myArray[9]);
 
 
     }
