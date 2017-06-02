4122c4fb8b5802c48242ba27c52589703a5dd4e3
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-06-02 16:50:22 +0100

    Revising composition

---
+++ revision/inheritance-composition/Guitar.java
@@ -0,0 +1,11 @@
+public class Guitar {
+
+    // In order to have both the play() method from MusicalInstrument
+    // And the burn() method from WoodenObject class
+    // I would use composition
+    
+    private MusicalInstrument mi = new MusicalInstrument();
+    private WoodenObject wo = new WoodenObject();
+
+
+}
---
+++ revision/inheritance-composition/MusicalInstrument.java
@@ -0,0 +1,5 @@
+public class MusicalInstrument {
+    public void play(String piece) {
+        System.out.println("Currently playing: " + piece);
+    }
+}
