e59095180189fb8d24ed77642caff759f5d19211
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-06-08 00:03:06 +0100

    Revising Streams

---
+++ Lambdas/src/Collections_Ex/Lambdas_02_03.java
@@ -0,0 +1,30 @@
+package Collections_Ex;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by ginestra on 08/06/2017.
+ */
+public class Lambdas_02_03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        IntStream.range(1, 4)
+                .forEach(System.out::println);
+
+        //find the average of the numbers squared
+        Arrays.stream(new int[]{1, 2, 3, 4})
+                .map(n -> n * n)
+                .average()
+                .ifPresent(System.out::println);
+
+        //map doubles to ints
+        Stream.of(1.5, 2.3, 3.7)
+                .mapToInt(Double::intValue)
+                .forEach(System.out::println);
+    }
+}
